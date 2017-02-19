baae2f85d8cfc9005e1c6217b05191ac523055c5
Dario Arias <h.dario95@gmail.com>
2017-02-18 19:24:55 -0500

    Vistas y estilos

---
+++ assets/js/codigo.js
@@ -0,0 +1,32 @@
+function setInputDate(_id){
+    var _dat = document.querySelector(_id);
+    var hoy = new Date(),
+        d = hoy.getDate(),
+        m = hoy.getMonth()+1, 
+        y = hoy.getFullYear(),
+        data;
+
+    if(d < 10){
+        d = "0"+d;
+    };
+    if(m < 10){
+        m = "0"+m;
+    };
+
+    data = y+"-"+m+"-"+d;
+    console.log(data);
+    _dat.value = data;
+};
+
+function updateInputDate(){
+    var _dat = document.querySelector("#dateDefault2");
+    var _dat1 = document.querySelector("#dateDefault");
+    data = _dat1.value;
+    console.log(data);
+    _dat.value = data;
+};
+
+
+
+setInputDate("#dateDefault");
+setInputDate("#dateDefault2");
